1166db82d4d66a3978c05cd919e974d98b0ba5e2
DomSacco <sacdom4273@gmail.com>
2023-05-29 12:29:39 +0200

    update function

--- Neuronet.py
+++ Neuronet.py
@@ -1,9 +1,9 @@
 #import numpy as np
-from Functions.Loss import mse
+from Functions.Loss import mse, mse_prime
 
 class Neuronet:
 
-    def __init__(self, *layers, learning_rate = 0.01, cost_fn = mse) -> None:
+    def __init__(self, *layers, learning_rate = 0.01, cost_fn = mse, cost_fn_prime = mse_prime) -> None:
         self.layers = list(layers)
         self.learning_rate = learning_rate
         self.cost_fn = cost_fn
@@ -12,6 +12,10 @@ class Neuronet:
         for l in self.layers:
             x = l.feedforward(x)
         return x
+    
+    def update(self, x):
+        for l in self.layers[::-1]:
+            x = l.backpropagation(x, self.learning_rate)
 
     def train(self, x_train, y_train, epochs, batch_size, verbose=True):
         for e in range(epochs):
@@ -28,8 +32,8 @@ class Neuronet:
 
                 if batch_size == count:
                     batch_count += 1
-                    print(f'Batch {batch_count} compleated')
-                    #self.update()
+                    #print(f'Batch {batch_count} compleated')
+                    self.update(mse_prime(y_pred, y))
                     count = 0
                 
             error /= len(x_train)
