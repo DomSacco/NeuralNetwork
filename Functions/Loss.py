fa9df4477353d84052f0366e6df8e45416b876d0
DomSacco <sacdom4273@gmail.com>
2023-05-29 12:40:55 +0200

    added binary cross entropy loss and its derivative

--- Functions/Loss.py
+++ Functions/Loss.py
@@ -1,7 +1,13 @@
 import numpy as np
 
-def mse(v, u):
-    return np.mean((v-u)**2)
+def mse(y_pred, y_act):
+    return np.mean((y_pred-y_act)**2)
 
-def mse_prime(u, v):
-    return 2*(u-v)
+def mse_prime(y_pred, y_act):
+    return 2*(y_pred-y_act)
+
+def bcel(y_pred, y_act):
+    return np.mean(-y_act * np.log(y_act) - (1 - y_act) * np.log(1 - y_pred))
+
+def bcel_prime(y_pred, y_act):
+    return ((1 - y_act) / (1 - y_pred) - y_act / y_pred)
