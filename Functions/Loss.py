7dfefac287eab0dd904835cf2416ba0caf75f75d
DomSacco <sacdom4273@gmail.com>
2023-05-29 13:13:25 +0200

    corrected bcel

--- Functions/Loss.py
+++ Functions/Loss.py
@@ -7,7 +7,7 @@ def mse_prime(y_pred, y_act):
     return 2*(y_pred-y_act)
 
 def bcel(y_pred, y_act):
-    return np.mean(-y_act * np.log(y_act) - (1 - y_act) * np.log(1 - y_pred))
+    return np.mean(-y_act * np.log(y_pred) - (1 - y_act) * np.log(1 - y_pred))
 
 def bcel_prime(y_pred, y_act):
     return ((1 - y_act) / (1 - y_pred) - y_act / y_pred)
