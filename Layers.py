bd460b508c8ddca341c11eb8dfbf4ccf6080b318
DomSacco <sacdom4273@gmail.com>
2023-05-29 11:39:59 +0200

    abstract class Layer

--- Layers.py
+++ Layers.py
@@ -1,6 +1,20 @@
 import numpy as np
+from abc import ABC, abstractmethod
 
-class Dense():
+class Layer(ABC):
+
+    def __init__(self) -> None:
+        self.input = None
+
+    @abstractmethod
+    def feedforwrd(self, input):
+        pass
+
+    @abstractmethod
+    def backpropagation(self, gradient_output, learning_rate):
+        pass
+
+class Dense(Layer):
 
     def __init__(self, input_size, output_size) -> None:
         self.weights = np.random.randn(output_size, input_size)
@@ -10,7 +24,7 @@ class Dense():
         self.input = input
         return self.weights @ input + self.biases 
 
-class Activation():
+class Activation(Layer):
 
     def __init__(self, fn) -> None:
         self.fn = fn
